94ac74e130cf293d8822fb6ad0a7620cebbd6686
FoysalHossain2 <fh375170@gmail.com>
2024-02-25 20:57:25 +0600

    FAQ create with React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-
 import { useState } from 'react'
 import './App.css'
+import {question} from './Data/Data'
 
 function App() {
 
-  const [modelStatus , setModalStatus] = useState(false)
-
+  const [showAns , setShowAns] = useState(question[0].id)
   
   return (
     <>   
+     <div className='w-[1320px] m-auto'>
+      <h1 className='text-center font-medium text-3xl'>Frequently Ask Questions FAQ </h1>
+       <div className='w-1/2 mt-10' >
+
+        {question.map((faqItems,i)=>{
+          
+        return(
+          <div className='faqItems border border-sky-500 mb-3'>
+          <h2 className='text-center bg-slate-600 py-3  cursor-pointer' onClick={()=>setShowAns(faqItems.id)}>
+            {faqItems.question}
+          </h2>
+          <p className={`text-center h-0 m-0 scale-0 transition-all ${showAns === faqItems.id ? 'active' : ''}`}
+             > 
+            {faqItems.answer}
+          </p>
+        </div>
+        )
+
+        })}
 
 
-
-<button className='btn bg-slate-600 p-2' onClick={()=>setModalStatus(true)} > Open Model </button>
-
-<div onClick={()=>setModalStatus(false)} className={`modelOverlay ${modelStatus ? 'ShowModal': ''}` } ></div>
-<div className={ `modelDiv1  ${modelStatus ? 'ShowModalDiv': ''}`} >
-  <h2 className='text-center'>
-    Enquiry <span onClick={()=>setModalStatus(false)} className='cursor-pointer'>&times;</span>
-  </h2>
-</div>
+       </div>
+     </div>
     </>
   )
 }
---
+++ src/Data/Data.jsx
@@ -0,0 +1,22 @@
+export const question = [
+    {
+        id: 1,
+        question: 'Hello world',
+        answer: '  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ipsum culpa ab animi temporibus earum, beatae voluptatibus repellat laborum dolorum excepturi, sapiente obcaecati rem ducimus fuga dolorem. Autem vel optio sint dolor modi, dicta non quis perferendis placeat veniam incidunt. Debitis eius natus sunt id nisi veritatis quia aperiam saepe.',
+    },
+    {
+        id: 2,
+        question: 'REACT JS Full Course',
+        answer: '  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ipsum culpa ab animi temporibus earum, beatae voluptatibus repellat laborum dolorum excepturi, sapiente obcaecati rem ducimus fuga dolorem. Autem vel optio sint dolor modi, dicta non quis perferendis placeat veniam incidunt. Debitis eius natus sunt id nisi veritatis quia aperiam saepe.',
+    },
+    {
+        id: 3,
+        question: 'Website Project',
+        answer: '  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ipsum culpa ab animi temporibus earum, beatae voluptatibus repellat laborum dolorum excepturi, sapiente obcaecati rem ducimus fuga dolorem. Autem vel optio sint dolor modi, dicta non quis perferendis placeat veniam incidunt. Debitis eius natus sunt id nisi veritatis quia aperiam saepe.',
+    },
+    {
+        id: 4,
+        question: 'Website Project',
+        answer: '  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ipsum culpa ab animi temporibus earum, beatae voluptatibus repellat laborum dolorum excepturi, sapiente obcaecati rem ducimus fuga dolorem. Autem vel optio sint dolor modi, dicta non quis perferendis placeat veniam incidunt. Debitis eius natus sunt id nisi veritatis quia aperiam saepe.',
+    },
+]
