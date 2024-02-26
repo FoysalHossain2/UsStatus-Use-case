529e1bfb5b0389c3f4061d640e9aeff1940abd1d
FoysalHossain2 <fh375170@gmail.com>
2024-02-26 20:39:55 +0600

    Todo practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,69 @@
 import { useState } from 'react'
 import './App.css'
-import {question} from './Data/Data'
 
 function App() {
 
-  const [showAns , setShowAns] = useState(question[0].id)
+  const [todoItem, setTodoItems] = useState([])
+
+  let todoSave = (e) => {
+    e.preventDefault()
+
+    let todoName = e.target.todoName.value
+    if (!todoItem.includes(todoName) ) {
+       let finalTodoData = [...todoItem,todoName]
+       setTodoItems(finalTodoData)
+    } else {
+      alert('exiting value')
+    }
+
+  }
+
+  let list = todoItem.map((value,index)=>(
+    <TodoListItems value={value} key={index} indexNumber={index} 
+    todoItem={todoItem}
+    setTodoItems={setTodoItems}
+     />
+  ))
+
   
   return (
     <>   
-     <div className='w-[1320px] m-auto'>
-      <h1 className='text-center font-medium text-3xl'>Frequently Ask Questions FAQ </h1>
-       <div className='w-1/2 mt-10' >
+      <div className=' flex flex-col justify-center items-center'>
+        <h1 className='text-2xl mt-[50px] mb-8'>Todo List</h1>
+        <form onSubmit={todoSave}>
+          <input type="text" name='todoName' className='py-3 bg-slate-300 w-[600px] pl-4 outline-none ' /> 
+          <button className='py-3 bg-green-200 px-4' >SAVE</button>
+        </form>
 
-        {question.map((faqItems,i)=>{
-          
-        return(
-          <div className='faqItems border border-sky-500 mb-3'>
-          <h2 className='text-center bg-slate-600 py-3  cursor-pointer' onClick={()=>setShowAns(faqItems.id)}>
-            {faqItems.question}
-          </h2>
-          <p className={`text-center h-0 m-0 scale-0 transition-all ${showAns === faqItems.id ? 'active' : ''}`}
-             > 
-            {faqItems.answer}
-          </p>
-        </div>
-        )
-
-        })}
-
-
-       </div>
-     </div>
+        <ul className='w-[670px] pl-4 mt-3 m-0 p-0 list-none ' >
+         {list}
+        </ul>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
+
+function TodoListItems({value, indexNumber, todoItem, setTodoItems}){
+
+  const [todoCakeData, setTodoCakeData] = useState(false)
+
+  let deletedRow = () => {
+   let finalData = todoItem.filter( (v,i) => i !=indexNumber)
+   setTodoItems(finalData);
+  }
+
+
+  let CakeData = () => {
+    setTodoCakeData(!todoCakeData)
+  }
+
+
+  return(
+    <li onClick={CakeData}  className= {`bg-slate-300 py-3 text-left text-[22px] relative mt-4 cursor-pointer ${(todoCakeData) ? 'completeItem' : ''}` }> 
+     {indexNumber + 1.} {value}
+      <span className='absolute right-3 cursor-pointer' onClick={deletedRow}> &times; </span> 
+   </li>
+  )
+}
